cfdbc8a76bc3c4a12af95ca6df25a6999b685a4c
Hewan2568 <84668659+Hewan2568@users.noreply.github.com>
2021-09-17 15:38:35 +0300

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 
 
-
-
-
 def simple_delete(a_dictionary, key=""):
 
-    
+    if a_dictionary.get(key):
+        del a_dictionary[key]
 
-if a_dictionary.get(key):
-
-del a_dictionary[key]
-
-                    
-
-return(a_dictionary)
+    return(a_dictionary)
